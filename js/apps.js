8f8b11ce37807cdb184f52c5230dee7719d12f1a
German27 <german.velasco98@gmail.com>
2018-11-08 11:00:47 -0300

    Correcion pautas de la funcion

--- js/apps.js
+++ js/apps.js
@@ -1,15 +1,15 @@
 
-function habilitarPerfil() {                             //Funcion con For solo para deshabilitar
+function habilitarPerfil() {                             //Funcion con For para habilitar inputs y deshabilitar boton editar perfil
   var inputs = document.getElementsByTagName('input');  
   for (var i = 0; i < inputs.length; i++) {              // Recorre los inputs y retorna los inputs desactivados como falso
     inputs[i].disabled = false;
   }
-  botonEditar = document.getElementById("botonEditar");
+  botonEditar = document.getElementById("botonEditar"); // Desactivo el boton de editar perfil
   botonEditar.disabled = "disabled";
 }
 
 // function botonToggle() {
-//  for (var i = 0; i < inputs.length; i++) {            //Funcion con For para habilitar/deshabilitar
+//  for (var i = 0; i < inputs.length; i++) {            //Funcion con For para habilitar/deshabilitar un boton
 //    inputs[i].disabled = !inputs[i].disabled;
 //  }
 // }
