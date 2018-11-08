e69d582c7deb33abf78dbf642ea35cd3a6ea54eb
German27 <german.velasco98@gmail.com>
2018-11-08 19:11:31 -0300

    Funcion descartar cambios

--- js/apps.js
+++ js/apps.js
@@ -15,3 +15,17 @@ function habilitarPerfil() {                             //Funcion con For para 
 //    inputs[i].disabled = !inputs[i].disabled;
 //  }
 // }
+
+function descartarCambios() {
+  var inputs = document.getElementsByTagName('input');
+  for (var i = 0; i < inputs.length; i++) {              
+    inputs[i].setAttribute("disabled", "disabled");           //En caso de descartar cambios, se bloquean de nuevo los campos
+  }
+    botonEditar = document.getElementById("botonEditar"); 
+    botonEditar.removeAttribute("disabled");                  //Vuelvo a habilitar el boton de editar perfil
+    botonDescartar = document.getElementById("botonDescartar");
+    botonDescartar.setAttribute("hidden", "hidden");          // Vuelvo a esconder el boton descartar
+
+   //////FALTA CANCELAR LOS CAMBIOS Y RETORNAR LOS CAMPOS A SUS VALORES ANTERIORES!!!!//////
+
+}
